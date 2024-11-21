7d37747fe1d7ed26c974e5993c144a5240933340
YounusEjazi <younasejazi7@icloud.com>
2024-11-21 16:16:29 +0100

    game logic and Navbar update

--- pdf-quiz-chatbot/src/components/Navbar.tsx
+++ pdf-quiz-chatbot/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image"; // Import for displaying the logo
 
 import UserAccountNav from "./UserAccountNav";
 import { ThemeToggle } from "./ThemeToggle";
@@ -9,7 +10,7 @@ import SignInButton from "./SignInButton";
 const Navbar = async () => {
   const session = await getAuthSession();
   return (
-    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300  py-2 ">
+    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
       <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
         {/* Logo */}
         <Link href={"/"} className="flex items-center gap-2">
@@ -17,7 +18,15 @@ const Navbar = async () => {
             PDF-Quiz-Chatbot
           </p>
         </Link>
-        <div className="flex items-center">
+        <div className="flex items-center gap-4">
+          {/* Adjusted the logo style */}
+          <Image
+            src="/Logo_Universität_Siegen.svg" // Path to your logo file
+            alt="Logo"
+            width={150}
+            height={100}
+            className="rounded-md dark:brightness-0 dark:invert"
+          />
           <ThemeToggle className="mr-4" />
           {session?.user ? (
             <UserAccountNav user={session.user} />
