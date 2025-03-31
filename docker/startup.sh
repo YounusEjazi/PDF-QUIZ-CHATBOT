03e74bf165ca43cb4bbfe68f7fea89223f5d5467
YounusEjazi <younasejazi7@icloud.com>
2025-03-31 18:02:54 +0200

    docker updates

--- docker/startup.sh
+++ docker/startup.sh
@@ -3,18 +3,17 @@
 # Ensure dependencies are installed (in case they are missing)
 export NODE_OPTIONS=--openssl-legacy-provider
 
-
 # Wait for PostgreSQL to be ready (check every 2 seconds for up to 60 seconds)
-echo "Waiting for PostgreSQL to be ready..."
-for i in {1..30}; do
-  if nc -z -v -w5 db 5432; then
-    echo "PostgreSQL is ready!"
-    break
-  else
-    echo "Waiting for PostgreSQL to start..."
-    sleep 2
-  fi
-done
+# echo "Waiting for PostgreSQL to be ready..."
+# for i in {1..30}; do
+#   if nc -z -v -w5 db 5432; then
+#     echo "PostgreSQL is ready!"
+#     break
+#   else
+#     echo "Waiting for PostgreSQL to start..."
+#     sleep 2
+#   fi
+# done
 
 # Run Prisma migrations (if necessary)
 echo "Running Prisma migrations..."
